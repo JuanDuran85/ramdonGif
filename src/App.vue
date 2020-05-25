510c74f857d67ddae8f34e6ddc9952c54236538d
JuanDuran85 <duran85juan@gmail.com>
2020-05-25 17:03:11 -0400

    desafio gato random

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Random Git Cat</h1>
+    <form>
+      <div class="form-group">
+        <label for="titulo">Titulo: </label>
+        <input type="text" class="form-control" v-model="titulo">
+      </div>
+      <div class="form-group">
+        <label for="filtro">Filtro:</label>
+        <select class="form-control" v-model="filtro">
+          <option value="blur">Desenfocado</option>
+          <option value="mono">Monocromático</option>
+          <option value="sepia">Sepia</option>
+          <option value="negative">Negativo</option>
+          <option value="paint">Efecto de Pintura</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="colores">Colores:</label>
+        <select class="form-control" v-model="colores">
+          <option value="red">Rojo</option>
+          <option value="green">Verde</option>
+          <option value="blue">Azul</option>
+          <option value="yellow">Amarillo</option>
+          <option value="white">Blanco</option>
+        </select>
+        <span :style="{backgroundColor: colores}">color de fondo</span>
+      </div>
+      <div class="form-group">
+        <label for="tamano">Tamaño: </label>
+        <input type="number" class="form-control" v-model="medida">
+      </div>
+      <button class="btn btn-primary" @click.prevent="buscar">Obtener mi gatito</button>
+    </form>
+    <HelloWorld :url="url"/>
   </div>
 </template>
 
@@ -10,19 +42,31 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      titulo: 'Gato',
+      filtro: 'blur',
+      colores: 'red',
+      medida: '300',
+      url: ''
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: { 
+    buscar(){
+      if (this.titulo && this.medida){
+        this.url = `https://cataas.com/cat/gif/says/${this.titulo}?filter=${this.filtro}&color=${this.colores}&size=${this.medida}`;
+        console.log(this.url);
+      } else {
+        alert("Ingrese valores validos");
+      }
+    } 
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
